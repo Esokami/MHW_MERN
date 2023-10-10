3a439f65e5f92c48075073f3d4d33f7223c27d5f
Esokami <mollica.es@gmail.com>
2023-10-10 15:08:48 -0700

    Trying to fix cannot get issue

--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join("client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    });
+}
+
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port: ${process.env.PORT}`)
 })
