ab8f52b0998571c53f0a25b30b1de4e6c100b5b8
Esokami <mollica.es@gmail.com>
2023-10-10 14:04:52 -0700

    updating to include mongo atlas

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost/${process.env.DB}`, {
+mongoose.connect(`mongodb://localhost/${process.env.MONGO_URI}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
