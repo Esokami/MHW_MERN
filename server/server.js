a6e0a3758098e7c1c835d077fb1d8e54e8f0ab02
Esokami <mollica.es@gmail.com>
2023-10-12 14:09:29 -0700

    Potential fix

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 const cookieParser = require('cookie-parser');
 const { application } = require('express');
+const path = require('path');
 
 app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 app.use(express.json());
