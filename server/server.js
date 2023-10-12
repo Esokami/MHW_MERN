d5742f5c21df32beef8437a305bca5defddf8888
Esokami <mollica.es@gmail.com>
2023-10-12 09:34:43 -0700

    Potential fix

--- server/server.js
+++ server/server.js
@@ -10,10 +10,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'client', 'src')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.js'))
+    });
+}
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(
+    PORT,
+    console.log(`Server running on ${process.env.NODE_ENV}`)
+);
 
 require('./config/mongoose.config');
 require('./routes/item.routes')(app);
