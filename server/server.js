21b7d6b3a22aa14eb806d816d3fbec8fffc74d3a
Esokami <mollica.es@gmail.com>
2023-10-12 15:06:33 -0700

    MongoDB connect fix

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || '8080'
 
 app.set("port", PORT);
 
-app.use(cors({origin: 'http://localhost:3000', credentials: true}));
+app.use(cors({origin: PORT, credentials: true}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
