d2d17c0bb4fb62f9d17af9e765a9755302c95cb3
Esokami <mollica.es@gmail.com>
2023-10-12 15:43:40 -0700

    MongoDB connect fix

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:8000', {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
