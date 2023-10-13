cf9f467d0bd0c9e8b53d544f2f14d8d0dfcbb83e
Esokami <mollica.es@gmail.com>
2023-10-13 12:59:26 -0700

    Updating gitignore

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://mollicaes:u8aYQPS8uUNK4FhP@monsterhunter-east.mrncrjx.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://mollicaes:dYveQWJplddVFItV@monsterhunter-east.mrncrjx.mongodb.net/?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
