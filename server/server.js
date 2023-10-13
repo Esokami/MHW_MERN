b0d8838b2138697d320adf255ae7e44043d93eab
Esokami <mollica.es@gmail.com>
2023-10-13 07:38:10 -0700

    .map potential fix

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '..', 'client/build')));
-    app.get("*", (req, res) => {
+    app.get(/^\/(?!api).*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
     });
 }
