8568c19c7106d04435c23932661f2234e8a7d21a
Esokami <mollica.es@gmail.com>
2023-10-13 13:46:48 -0700

    updating config

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,6 +1,5 @@
-require('dotenv').config();
 const mongoose = require('mongoose');
-const URI = process.env.MONGODB_URI;
+export const URI = process.env.MONGODB_URI;
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
